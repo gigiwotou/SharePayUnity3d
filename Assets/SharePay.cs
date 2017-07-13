83ec516c933b14df560661b489c7a755cfaf6434
gigiwotou <gigiwotou@sohu.com>
2017-07-14 00:13:01 +0800

    1
    1

--- Assets/SharePay.cs
+++ Assets/SharePay.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class SharePay : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    AndroidJavaObject sharePay;
+    public string appid;
+
+    // Use this for initialization
+    void Start () {
+        sharePay = new AndroidJavaObject("com.wotou.SharePay.WeChatShare");
+        sharePay.Call("initappid", transform.name, appid, 1);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void ShareWX()
+    {
+        sharePay.Call("shareToWeChat", "微信分享", "www.163.com", "蛇王争霸");
+    }
 }
